66728a172281f09a0de08a2448435ee6c60c5fea
HamoHamoHamo <hamo222@naver.com>
2021-02-01 14:56:02 +0000

    21.02.01 - dispatc url을 generic view 형식으로 바꿈

--- dispatch/urls.py
+++ dispatch/urls.py
@@ -5,17 +5,18 @@ app_name = 'dispatch'
 
 urlpatterns = [
     #path('<str:kinds>/', views.NoticeKindsView.as_view(), name='kinds'),
-    path('', views.home, name='home'),
-    path('order/', views.order, name='order'),
+    path('', views.DispatchList.as_view(), name='home'),
+    path('order/', views.OrderList.as_view(), name='order'),
     path('order/create/', views.order_create, name='order_create'),
-    path('order/<int:order_id>/', views.order_detail, name='order_detail'),
-    path('order/<int:order_id>/delete/', views.order_delete, name='order_delete'),
-    path('order/<int:order_id>/edit/', views.order_edit, name='order_edit'),
+    path('order/<int:pk>/', views.OrderDetail.as_view(), name='order_detail'),
+    path('order/<int:pk>/delete/', views.order_delete, name='order_delete'),
+    path('order/<int:pk>/edit/', views.OrderEdit.as_view(), name='order_edit'),
 
-    path('schedule/', views.schedule, name='schedule'),
+    path('schedule/', views.ScheduleList.as_view(), name='schedule'),
+    path('schedule/<int:date>/', views.ScheduleDetail.as_view(), name='schedule_detail'),
 
-    path('management/', views.management, name='management'),
-    path('management/<int:order_id>/', views.management_detail, name='management_detail'),
-    path('management/<int:order_id>/create/', views.management_create, name='management_create'),
-    path('management/<int:order_id>/edit/', views.management_edit, name='management_edit'),
+    path('management/', views.ManagementList.as_view(), name='management'),
+    path('management/<int:pk>/', views.ManagementDetail.as_view(), name='management_detail'),
+    path('management/<int:pk>/create/', views.management_create, name='management_create'),
+    path('management/<int:pk>/edit/', views.management_edit, name='management_edit'),
 ]
