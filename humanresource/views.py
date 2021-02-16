73d4d673859fe7b5585bf5a5d1b41dcb1b466412
HamoHamoHamo <hamo222@naver.com>
2021-02-16 13:10:29 +0000

    21.02.15 - hr 페이징 구현

--- humanresource/views.py
+++ humanresource/views.py
@@ -12,10 +12,29 @@ class ManagementList(generic.ListView):
     template_name = 'HR/management.html'
     context_object_name = 'HR_list'
     model = HR
+    paginate_by = 10
 
     def get_queryset(self):
         HR_list = HR.objects.order_by('-start_date')
         return HR_list
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        paginator = context['paginator']
+        page_numbers_range = 5
+        max_index = len(paginator.page_range)
+        page = self.request.GET.get('page')
+        current_page = int(page) if page else 1
+
+        start_index = int((current_page - 1) / page_numbers_range) * page_numbers_range
+        end_index = start_index + page_numbers_range
+        if end_index >= max_index:
+            end_index = max_index
+
+        page_range = paginator.page_range[start_index:end_index]
+        context['page_range'] = page_range
+
+        return context
 
 def HR_create(request):
     context = {}
--- vehicle/models.py
+++ vehicle/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from crudmember.models import User
+from humanresource.models import Member
 
-# Create your models here.
+class Vehicle(models.Model):
+    vehicle_num = models.CharField(verbose_name='차량번호', max_length=15, null=False)
+    group = models.CharField(verbose_name='소속', max_length=15, null=False)
+    vehicle_type = models.CharField(verbose_name='차량종류', max_length=15, null=False)
+    maker = models.CharField(verbose_name='제조사', max_length=15, null=False)
+    model_year = models.CharField(verbose_name='연식', max_length=15, null=False)
+    driver = models.ForeignKey(Member, verbose_name='기사', on_delete=models.SET_NULL, null=True, related_name="driver", db_column="driver")
+    use = models.BooleanField(verbose_name='사용여부', default=False)
+    passenger_num = models.IntegerField(verbose_name='승차인원', null=False)
+
+    def __str__(self):
+        return self.vehicle_num
+
