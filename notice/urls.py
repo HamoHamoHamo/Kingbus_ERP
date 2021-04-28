f78ef37aaf78a444a78687f8fbd6cb1b108ea244
HamoHamoHamo <hamo222@naver.com>
2021-04-28 15:35:14 +0000

    21.04.28 - notice css top_box_selector 수정중

--- notice/urls.py
+++ notice/urls.py
@@ -5,7 +5,6 @@ app_name = 'notice'
 
 urlpatterns = [
     #path('<str:kinds>/', views.NoticeKindsView.as_view(), name='kinds'),
-    path('', views.home, name='home'),
     path('create/', views.create, name='create'),
     path('<str:kinds>/', views.kinds, name='kinds'),
     path('<str:kinds>/<int:pk>/', views.NoticeDetail.as_view(), name='detail'),
