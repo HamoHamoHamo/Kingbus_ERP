f735caad08c78819cc238eb4505a0128d5c0bc84
HamoHamoHamo <hamo222@naver.com>
2023-06-12 18:10:34 +0900

    차량정비신청관리 완료
    상담신청에도 사진 추가,

--- complaint/admin.py
+++ complaint/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Consulting, VehicleInspectionRequest
+from .models import Consulting, VehicleInspectionRequest, InspectionRequestFile, ConsultingFile
 
 admin.site.register(Consulting)
 admin.site.register(VehicleInspectionRequest)
+admin.site.register(InspectionRequestFile)
+admin.site.register(ConsultingFile)
