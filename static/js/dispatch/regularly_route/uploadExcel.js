6378dc5299b9fdc5798110a3ce17a95ae7586bb5
HamoHamoHamo <hamo222@naver.com>
2023-07-20 14:27:39 +0900

    출/퇴근 노선관리 - 엑셀 다운로드 기능 추가, 엑셀 업로드 수정

--- static/js/dispatch/regularly_route/uploadExcel.js
+++ static/js/dispatch/regularly_route/uploadExcel.js
@@ -41,7 +41,7 @@ function readExcel() {
     let reader = new FileReader();
     reader.onload = function () {
         let data = reader.result;
-        let workBook = XLSX.read(data, { type: 'binary', cellDates: true, dateNF: 'yyyy-mm-dd' });
+        let workBook = XLSX.read(data, { type: 'binary' });
         workBook.SheetNames.forEach(function (sheetName) {
             console.log('SheetName: ' + sheetName);
             let rows = XLSX.utils.sheet_to_json(workBook.Sheets[sheetName]);
@@ -62,44 +62,65 @@ function dateFormat(e) {
     if (uploadState) {
 
         uploadState = false;
-
         // 유효성검사
+        //console.log("TEST", excelData);
 
-        // 순번
+        
+        const regex = /^(0[0-9]|1[0-9]|2[0-3]):(0[1-9]|[0-5][0-9])$/;
+        let column;
+
         for (i = 0; i < excelData.length; i++) {
-            if (!`${excelData[i].순번}`.includes(',')) {
+            if (excelData[i]["그룹"] == undefined ||
+                excelData[i]["노선명"] == undefined ||
+                excelData[i]["출발지"] == undefined ||
+                excelData[i]["도착지"] == undefined ||
+                excelData[i]["순번1"] == undefined ||
+                excelData[i]["순번2"] == undefined ||
+                excelData[i]["출발시간"] == undefined ||
+                excelData[i]["도착시간"] == undefined ||
+                excelData[i]["출/퇴근"] == undefined ||
+                excelData[i]["운행요일"] == undefined ||
+                excelData[i]["금액"] == undefined ||
+                excelData[i]["기사수당"] == undefined ||
+                excelData[i]["사용"] == undefined)
+            {
+                uploadState = true;
+                return alert(`${i + 1}번째 데이터의 필수 입력 사항이 입력되지 않았습니다.`)
+            }
+            
+            // 운행시간
+            if (!(regex.test(excelData[i].출발시간)) || !(regex.test(excelData[i].도착시간))) {
                 excelUploadFile.value = ""
                 excelUploadFileText.value = ""
-                return alert(`${i + 1}번째 순번에 ' , '를 입력하지 않았습니다.`)
+                uploadState = true;
+                return alert(`${i + 1}번째 데이터의 운행시간이 형식에 맞지 않습니다.`)
             }
-        };
-        // 운행시간
-        const regex = /\d{2}:\d{2}~\d{2}:\d{2}/;
-        for (i = 0; i < excelData.length; i++) {
-            if (!regex.test(excelData[i].운행시간)) {
-                excelUploadFile.value = ""
-                excelUploadFileText.value = ""
-                return alert(`${i + 1}번째 운행시간이 형식에 맞지 않습니다.`)
-            }
-        };
-        // 출/퇴근
-        for (i = 0; i < excelData.length; i++) {
+            // 출/퇴근
             if (excelData[i]["출/퇴근"] !== "출근" && excelData[i]["출/퇴근"] !== "퇴근") {
                 excelUploadFile.value = ""
                 excelUploadFileText.value = ""
-                return alert(`${i + 1}번째 출/퇴근 항목이 형식에 맞지 않습니다.`)
+                uploadState = true;
+                return alert(`${i + 1}번째 데이터의 출/퇴근 항목이 형식에 맞지 않습니다.`)
             }
-        };
-        // 운행요일
-        for (i = 0; i < excelData.length; i++) {
-            let weekCheck = excelData[i].운행요일.replace(/\ /g, "")
+            // 운행요일
+            let weekCheck = excelData[i].운행요일.split(" ");
             for (j = 0; j < weekCheck.length; j++) {
                 if (weekCheck[j] !== "월" && weekCheck[j] !== "화" && weekCheck[j] !== "수" && weekCheck[j] !== "목" && weekCheck[j] !== "금" && weekCheck[j] !== "토" && weekCheck[j] !== "일") {
                     excelUploadFile.value = ""
                     excelUploadFileText.value = ""
-                    return alert(`${i + 1}번째 운행요일 항목이 형식에 맞지 않습니다.`)
+                    uploadState = true;
+                    return alert(`${i + 1}번째 데이터의 운행요일 항목이 형식에 맞지 않습니다.`)
                 }
             };
+            console.log("TEST", excelData[i]["사용"])
+            if (excelData[i]["사용"] !== '사용' && excelData[i]["사용"] !== '미사용')
+            {
+                excelUploadFile.value = ""
+                excelUploadFileText.value = ""
+                uploadState = true;
+                return alert(`${i + 1}번째 데이터의 사용 항목이 형식에 맞지 않습니다.`)
+            }
+            
         };
 
 
@@ -107,36 +128,29 @@ function dateFormat(e) {
 
         let formatingData = []
         for (i = 0; i < excelData.length; i++) {
-            let driveWeek = ""
-            for (j = 0; j < excelData[i].운행요일.length; j++) {
-                if (j == excelData[i].운행요일.length - 1) {
-                    driveWeek = `${driveWeek}` + `${excelData[i].운행요일[j]}`
-                } else {
-                    driveWeek = `${driveWeek}` + `${excelData[i].운행요일[j]}` + " "
-                }
-            };
-            let nembering = excelData[i].순번.replace(/\ /g, "")
-
             let formatingObj = {
-                group: excelData[i].그룹,
-                route: excelData[i].노선명,
-                departure: excelData[i].출발지,
-                arrival: excelData[i].도착지,
-                number1: nembering.split(",")[0],
-                number2: nembering.split(",")[1],
-                departure_time: excelData[i].운행시간.split("~")[0],
-                arrival_time: excelData[i].운행시간.split("~")[1],
+                id: excelData[i]["id"] == undefined ? "" : excelData[i]["id"],
+                group: excelData[i]["그룹"],
+                route: excelData[i]["노선명"],
+                departure: excelData[i]["출발지"],
+                arrival: excelData[i]["도착지"],
+                number1: excelData[i]["순번1"],
+                number2: excelData[i]["순번2"],
+                departure_time: excelData[i]["출발시간"],
+                arrival_time: excelData[i]["도착시간"],
                 work_type: excelData[i]["출/퇴근"],
-                location: excelData[i].위치 == undefined ? "" : excelData[i].위치,
-                week: driveWeek,
-                detailed_route: excelData[i].상세노선 == undefined ? "" : excelData[i].상세노선,
-                price: excelData[i].금액,
-                driver_allowance: excelData[i].기사수당,
-                references: excelData[i].참조사항 == undefined ? "" : excelData[i].참조사항,
+                location: excelData[i]["위치"] == undefined ? "" : excelData[i]["위치"],
+                week: excelData[i]['운행요일'],
+                detailed_route: excelData[i]["상세노선"] == undefined ? "" : excelData[i]["상세노선"],
+                maplink: excelData[i]["카카오맵"] == undefined ? "" : excelData[i]["카카오맵"],
+                waypoint: excelData[i]["경유지"] == undefined ? "" : excelData[i]["경유지"],
+                price: excelData[i]["금액"],
+                driver_allowance: excelData[i]["기사수당"],
+                references: excelData[i]["참조사항"] == undefined ? "" : excelData[i]["참조사항"],
+                use: excelData[i]["사용"],
             }
             formatingData.push(formatingObj)
         };
-
         console.log("DATAA", formatingData);
         $.ajax({
             url: "/dispatch/regularly/route/upload",
@@ -147,12 +161,20 @@ function dateFormat(e) {
                 visibleLoading.style.display = "none"
                 uploadState = true
                 console.log("DATASS", data);
-                if (data['group_error']) {
-                    alert(`${data['line']}번째 그룹 항목 ${data['group_error']} 가 그룹목록에 존재하지 않습니다.`)
+                if (data['error'] == 'group') {
+                    alert(`${data['line']}번째 데이터의 그룹 항목 ${data['group_error']} 가 그룹목록에 존재하지 않습니다.`)
                     excelUploadFile.value = ""
                     excelUploadFileText.value = ""
+                    uploadState = true;
                     return;
-                } else if (data['count']) {
+                } else if (data['error'] == 'required') {
+                    alert(`${data['line']}번째 데이터의 필수 입력 사항이 입력되지 않았습니다.`)
+                    excelUploadFile.value = ""
+                    excelUploadFileText.value = ""
+                    uploadState = true;
+                    return;
+                }
+                else if (data['count']) {
                     alert(`${data['count']}개의 데이터가 저장되었습니다`);
 
                     location.reload();
