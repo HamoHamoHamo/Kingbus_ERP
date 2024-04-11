7e574a9942d33f885c2e188e6a05350e47bbd02d
webdevs-lee <jsweb1008@gmail.com>
2024-04-11 10:39:35 +0900

    FEAT: 인당효율 그래프 삭제 및 사고관리 UI 추가

--- static/js/vehicle/accident.js
+++ static/js/vehicle/accident.js
@@ -1,30 +1,8 @@
-// MARK: - 차트 세팅
-// const totalChart = document.querySelector(".total-run-pie-chart");
-// const totalChartMainValue = document.querySelector(".total-run-pie-chart-center")
-
-// const personalChart = document.querySelector(".personal-run-pie-chart")
-// const personalChartMainValue = document.querySelector(".personal-run-pie-chart-center")
-
-// const setTotalChart = (percent) => {
-//     totalChart.style.background = `conic-gradient(#3D36FF 25% ${percent}%, #FF8585 0% ${100 - percent}%)`
-//     totalChartMainValue.textContent = `${percent}%`
-
-// }
-
-// const setPersonalChart = (percent) => {
-//     personalChart.style.background = `conic-gradient(#3D36FF 25% ${percent}%, #EFFF8D 0% ${100 - percent}%)`
-//     personalChartMainValue.textContent = `${percent}%`
-
-// }
-
-// setTotalChart(54);
-// setPersonalChart(86);
-
 // MARK: - Today, DayBefore, DayAfter Button
 const setToday = () => {
     const today = new Date();
 
-    document.querySelector(".search-dispatch-path-date").value = today.toISOString().substring(0,10);
+    document.querySelector(".search-dispatch-path-date").value = today.toISOString().substring(0, 10);
 }
 
 const setDayBefore = () => {
@@ -49,7 +27,7 @@ const setDayAfter = () => {
     } else {
         date = new Date(currentDate);
     }
-    
+
     const tomorrow = new Date(date.setDate(date.getDate() + 1));
 
     document.querySelector(".search-dispatch-path-date").value = tomorrow.toISOString().substring(0, 10);
