42b2af8519c3904ce292bfe41774360a3899a65e
HamoHamoHamo <hamo222@naver.com>
2024-05-03 12:05:08 +0900

    fix: 홈페이지 메인에서 사이트에서 나가시겠습니까 alert 뜨는 오류 수정
    refreshTop.js에서 beforeunload 이벤트 사용해서 alert 떴음
    window.onload 사용하게 수정, thiredSectionAnimation.js에 있던 window.onload 가져와서 합침

--- static/js/homepage/refreshTop.js
+++ static/js/homepage/refreshTop.js
@@ -1,8 +1,20 @@
-// 새로고침 화면 상단이동
-window.addEventListener('beforeunload', (event) => {
-    event.preventDefault();
-    setTimeout(function () {
-        scrollTo(0, 0)
-    }, 0);
-  });
+// // 새로고침 화면 상단이동
+// window.addEventListener('beforeunload', (event) => {
+//     event.preventDefault();
+//     setTimeout(function () {
+//         scrollTo(0, 0)
+//     }, 0);
+//   });
+const body = document.querySelector("body")
 
+window.onload = function () {
+    if (window.innerWidth < 768) {
+        body.style.height  = "100%"
+        body.style.overflow = "hidden"
+        body.style.touchAction  = "none"
+    }
+
+    setTimeout(function () {
+        window.scrollTo({top: 0, behavior: "smooth"})
+    }, 100);
+}
