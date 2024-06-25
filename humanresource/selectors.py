57f8d4ea077607c8042425ae2f1dfa4020593758
HamoHamoHamo <hamo222@naver.com>
2024-06-26 00:41:27 +0900

    fix: MemberSelector get_using_driver_list 임시는 안 가져오게 수정

--- humanresource/selectors.py
+++ humanresource/selectors.py
@@ -4,7 +4,7 @@ from .models import Member, Salary
 
 class MemberSelector:
     def get_using_driver_list(self, name):
-        member = Member.objects.filter(use='사용').filter(Q(role='팀장')|Q(role='운전원')|Q(role='용역')|Q(role='임시')).order_by('name')
+        member = Member.objects.filter(use='사용').filter(Q(role='팀장')|Q(role='운전원')|Q(role='용역')).order_by('name')
         if name:
             member = member.filter(name__startswith=name)
         return member
