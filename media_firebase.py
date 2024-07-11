863ce7c49e4406853089206b0286bdfee1062562
HamoHamoHamo <hamo222@naver.com>
2024-07-11 10:10:48 +0900

    feat: init_firebaes에서 bucket 확인하고 다시 초기화 하는 부분 추가

--- media_firebase.py
+++ media_firebase.py
@@ -1,21 +1,51 @@
 from datetime import datetime, timedelta
 from pathlib import Path
-from firebase_admin import credentials, initialize_app, storage, _apps
+from firebase_admin import credentials, initialize_app, storage, _apps, get_app, delete_app
 import os
 from config.settings import BASE_DIR
 from my_settings import CRED_PATH, STORAGE_BUCKET, CLOUD_MEDIA_PATH
+from config.custom_logging import logger
+
+def is_correct_storage_bucket(app, expected_bucket):
+    try:
+        bucket = storage.bucket(app=app)
+        return bucket.name == expected_bucket
+    except Exception as e:
+        logger.error(f"Error checking storage bucket: {e}")
+        return False
+
 
 def init_firebase():
     cred = credentials.Certificate(os.path.join(BASE_DIR, CRED_PATH))
     if not _apps:
+        logger.info(f"storage_bucket {STORAGE_BUCKET}")
         initialize_app(cred, {
             'storageBucket': STORAGE_BUCKET,
         })
 
+    try:
+        app = get_app()
+        if not is_correct_storage_bucket(app, STORAGE_BUCKET):
+            logger.info("Existing Firebase app has incorrect storage bucket. Reinitializing...")
+            delete_app(app)
+            initialize_app(cred, {
+                'storageBucket': STORAGE_BUCKET,
+            })
+            logger.info(f"Firebase initialized with storage bucket: {STORAGE_BUCKET}")
+        else:
+            logger.info("Existing Firebase app has correct storage bucket.")
+    except ValueError:
+        # Firebase app has not been initialized yet
+        initialize_app(cred, {
+            'storageBucket': STORAGE_BUCKET,
+        })
+        logger.info(f"Firebase initialized with storage bucket: {STORAGE_BUCKET}")
+
+
 def upload_to_firebase(file, file_path):
     init_firebase()
-
     bucket = storage.bucket()
+    logger.info(f"bucket {bucket}, _apps {_apps}")
     blob = bucket.blob(file_path)
     blob.upload_from_filename(file.file.path)
 
