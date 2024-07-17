8a692ccdd466c86a271eb69931911c2d348f93c9
HamoHamoHamo <hamo222@naver.com>
2024-07-17 10:53:09 +0900

    feat: 임금테이블 항목 수정, 계산 식 수정

---
+++ common/formatter.py
@@ -0,0 +1,8 @@
+def format_number_with_commas(number):
+    """
+    주어진 숫자를 천 단위로 ','를 찍어서 문자열로 반환합니다.
+    """
+    if isinstance(number, (int, float)):
+        return f"{number:,}"
+    else:
+        raise ValueError("숫자 형식의 값만 입력 가능합니다.")
--- salary/models.py
+++ salary/models.py
@@ -6,32 +6,21 @@ class HourlyWage(models.Model):
         horuly_wage = HourlyWage.objects.create(
             wage1 = 0,
             wage2 = 0,
-            wage3 = 0,
-            wage4 = 0,
             month = month,
             creator = creator
         )
         return horuly_wage
 
     def get_wage(self, minute):
-        wage1_condition = 60 * 4
-        wage2_condition = 60 * 6
-        wage3_condition = 60 * 8
+        wage_condition = 60 * 30
 
-        if minute <= wage1_condition:
+        if minute < wage_condition:
             return self.wage1
-        if minute <= wage2_condition:
+        else:
             return self.wage2
-        if minute <= wage3_condition:
-            return self.wage3
-        if minute > wage3_condition:
-            return self.wage4
-        return None
 
-    wage1 = models.CharField(verbose_name='4시간 이하', max_length=40, null=False, default='0')
-    wage2 = models.CharField(verbose_name='4시간 초과 6시간 이하', max_length=40, null=False, default='0')
-    wage3 = models.CharField(verbose_name='6시간 초과 8시간 이하', max_length=40, null=False, default='0')
-    wage4 = models.CharField(verbose_name='8시간 초과', max_length=40, null=False, default='0')
+    wage1 = models.CharField(verbose_name='주 30시간 미만', max_length=40, null=False, default='0')
+    wage2 = models.CharField(verbose_name='주 30시간 이상', max_length=40, null=False, default='0')
     month = models.CharField(verbose_name='월', max_length=7, null=False)
     creator = models.ForeignKey(Member, on_delete=models.SET_NULL, related_name="hourly_wage", null=True)
     pub_date = models.DateTimeField(verbose_name='작성시간', auto_now_add=True, null=False)
