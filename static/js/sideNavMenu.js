8a1717762acf045bc5c09e81b83520473749a235
HamoHamoHamo <hamo222@naver.com>
2024-09-07 22:29:40 +0900

    feat: 사이드바 수정
    사이드바 2depth로 수정
    팀장 권한 화면 운전원이랑 같게 변경

--- static/js/sideNavMenu.js
+++ static/js/sideNavMenu.js
@@ -1,105 +1,61 @@
-const sideNavControll = document.querySelectorAll(".nav1BoxMenuHaveDepth2 .sideMenuArrow")
-const nav1BoxMenuHaveDepth2 = document.querySelectorAll(".nav1BoxMenuHaveDepth2")
-const sideMenuNav2Box = document.querySelectorAll(".sideMenuNav2Box")
-const sideMenuNav2 = document.querySelectorAll(".sideMenuNav2")
-const nav1Box = document.querySelectorAll(".nav1Box")
+const sideNavControll = document.querySelectorAll(".nav1BoxMenuHaveDepth2 .sideMenuArrow");
+const nav1BoxMenuHaveDepth2 = document.querySelectorAll(".nav1BoxMenuHaveDepth2");
+const sideMenuNav2Box = document.querySelectorAll(".sideMenuNav2Box");
+const sideMenuNav2 = document.querySelectorAll(".sideMenuNav2");
+const nav1Box = document.querySelectorAll(".nav1Box");
 
 let overlapCount = 111;
 let navOpenCount = false;
 
-for (i = 0; i < nav1BoxMenuHaveDepth2.length; i++) {
-  nav1BoxMenuHaveDepth2[i].addEventListener("click", openSideNav)
+for (let i = 0; i < nav1BoxMenuHaveDepth2.length; i++) {
+  nav1BoxMenuHaveDepth2[i].addEventListener("click", openSideNav);
 }
 
 function openSideNav() {
   const idx = Array.from(nav1BoxMenuHaveDepth2).indexOf(this);
-  for (i = 0; i < nav1BoxMenuHaveDepth2.length; i++) {
+  for (let i = 0; i < nav1BoxMenuHaveDepth2.length; i++) {
     sideMenuNav2Box[i].style.height = "0";
-    sideMenuNav2[i].style.height = "0"
-    sideNavControll[i].style.transform = "rotate(0deg)"
-    nav1Box[i+1].style.backgroundColor = "#1C1A4E";
+    sideMenuNav2[i].style.height = "0";
+    sideNavControll[i].style.transform = "rotate(0deg)";
+    nav1Box[i + 1].style.backgroundColor = "#1C1A4E";
   }
-  if (overlapCount == idx) {
+  
+  if (overlapCount === idx) {
     sideMenuNav2Box[idx].style.height = "0";
-    sideMenuNav2[idx].style.height = "0"
+    sideMenuNav2[idx].style.height = "0";
     overlapCount = 111;
     navOpenCount = false;
   } else {
     switch (idx) {
       case 0:
-        if (AUTHORITY == 3)
-          sideMenuNav2Box[idx].style.height = "18.4rem";
-        else
-          sideMenuNav2Box[idx].style.height = "10.4rem";
-        sideMenuNav2[idx].style.height = "8.4rem";
+        sideMenuNav2Box[idx].style.height = "10.6rem";
+        sideMenuNav2[idx].style.height = "8.6rem";
         break;
       case 1:
-        sideMenuNav2Box[idx].style.height = "43.8rem";
-        sideMenuNav2[idx].style.height = "41.8rem";
+        if (typeof AUTHORITY !== "undefined" && AUTHORITY === 3)
+          sideMenuNav2Box[idx].style.height = "18.4rem";
+        else
+          sideMenuNav2Box[idx].style.height = "14.4rem";
+        sideMenuNav2[idx].style.height = "12.4rem";
         break;
       case 2:
-        sideMenuNav2Box[idx].style.height = "14.6rem";
-        sideMenuNav2[idx].style.height = "12.6rem";
+        sideMenuNav2Box[idx].style.height = "22.6rem";
+        sideMenuNav2[idx].style.height = "20.6rem";
         break;
       case 3:
         sideMenuNav2Box[idx].style.height = "10.4rem";
         sideMenuNav2[idx].style.height = "8.4rem";
         break;
       case 4:
-        sideMenuNav2Box[idx].style.height = "48.2rem";
-        sideMenuNav2[idx].style.height = "46.2rem";
+        sideMenuNav2Box[idx].style.height = "18.4rem";
+        sideMenuNav2[idx].style.height = "16.4rem";
         break;
-      case 5:
-        sideMenuNav2Box[idx].style.height = "23rem";
-        sideMenuNav2[idx].style.height = "21rem";
-        break;
-      // case 4:
-      //   sideMenuNav2Box[idx].style.height = "15.2rem";
-      //   sideMenuNav2[idx].style.height = "13.2rem";
-      //   break;
-      // case 4:
-      //   sideMenuNav2Box[idx].style.height = "10.4rem";
-      //   sideMenuNav2[idx].style.height = "8.4rem";
-      //   break;
-      case 6:
-        sideMenuNav2Box[idx].style.height = "23rem";
-        sideMenuNav2[idx].style.height = "21rem";
-        break;
-      case 7:
-        sideMenuNav2Box[idx].style.height = "10.4rem";
-        sideMenuNav2[idx].style.height = "8.4rem";
-        break;
-      case 8:
-        sideMenuNav2Box[idx].style.height = "10.4rem";
-        sideMenuNav2[idx].style.height = "8.4rem";
-        break;
-      case 9:
-        sideMenuNav2Box[idx].style.height = "5.4rem";
-        sideMenuNav2[idx].style.height = "4rem";
-        break;
+      
     }
-    sideNavControll[idx].style.transform = "rotate(180deg)"
+
+    sideNavControll[idx].style.transform = "rotate(180deg)";
     this.parentNode.style.backgroundColor = "#19173B";
     overlapCount = idx;
     navOpenCount = true;
-  }
-}
-
-
-//hover 색 변화
-for (i = 0; i < nav1Box.length; i++) {
-  nav1Box[i].addEventListener('mouseover', bgThis)
-  nav1Box[i].addEventListener('mouseout', bgNotThis)
-}
-
-function bgThis() {
-  if (!navOpenCount) {
-    this.style.backgroundColor = "#19173B"
-  }
-}
-
-function bgNotThis() {
-  if (!navOpenCount) {
-    this.style.backgroundColor = "#1C1A4E"
   }
 }
