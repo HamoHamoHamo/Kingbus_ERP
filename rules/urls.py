7fbb4fe5d006795b61632fdfe00b6546cbc06230
wontak <wt060408@gmail.com>
2024-10-11 16:30:32 +0900

    규칙url /제거

--- rules/urls.py
+++ rules/urls.py
@@ -4,12 +4,12 @@ from . import views
 appname= 'rules'
 
 urlpatterns = [
-    path('approvalrules/', views.ApprovalRules.as_view(), name='approvalrules'),
-    path('rowcallrules/', views.RowCallRules.as_view(), name='rowcallrules'),
-    path('driverrules/', views.DriverRules.as_view(), name='driverrules'),
-    path('managerrules/', views.ManagerRules.as_view(), name='managerrules'),
-    path('fieldmanagerrules/', views.FieldManagerRules.as_view(), name='fieldmanagerrules'),
-    path('personnelcommittee/', views.PersonnelCommittee.as_view(), name='personnelcommittee'),
-    path('Itemmanagement/', views.ItemManagement.as_view(), name='Itemmanagement'),
-    path('organizationchart/', views.OrganizationChart.as_view(), name='organizationchart'),
+    path('approvalrules', views.ApprovalRules.as_view(), name='approvalrules'),
+    path('rowcallrules', views.RowCallRules.as_view(), name='rowcallrules'),
+    path('driverrules', views.DriverRules.as_view(), name='driverrules'),
+    path('managerrules', views.ManagerRules.as_view(), name='managerrules'),
+    path('fieldmanagerrules', views.FieldManagerRules.as_view(), name='fieldmanagerrules'),
+    path('personnelcommittee', views.PersonnelCommittee.as_view(), name='personnelcommittee'),
+    path('Itemmanagement', views.ItemManagement.as_view(), name='Itemmanagement'),
+    path('organizationchart', views.OrganizationChart.as_view(), name='organizationchart'),
 ]
