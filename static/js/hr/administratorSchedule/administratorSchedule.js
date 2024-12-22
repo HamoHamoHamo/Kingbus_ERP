e21e9af5a0495ad3af8cd1eacde365a7fff759e7
wontak <wt060408@gmail.com>
2024-12-22 22:44:04 +0900

    임금테이블 연차수당 html, css 수정

--- static/js/hr/administratorSchedule/administratorSchedule.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll(".progressBtnJs").forEach(button => {
-    button.addEventListener("click", () => {
-        const action = button.textContent.trim();
-        const isConfirmed = confirm(`되돌리기가 불가능합니다. ${action}하시겠습니까?`);
-    });
-});
